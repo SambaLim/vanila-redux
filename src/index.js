fd137f7439bcc3b0ef5482eb27cefb8228ac059c
Sungho Lim <sungho.lim@deliveryhero.co.kr>
2021-11-29 23:57:34 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const plus = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-const countModifierReducer = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") return count + 1;
+  if (action.type === "MINUS") return count - 1;
+  return count;
 };
-const countStore = createStore(countModifierReducer);
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
