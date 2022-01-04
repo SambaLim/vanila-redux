161024a3483406366c0905d2f4a200c9217f7986
Sungho Lim <sungho.lim@deliveryhero.co.kr>
2022-01-04 23:18:11 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,19 +1,15 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addTodo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteTodo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+const reducer = createReducer([], {
+  [addTodo.type]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteTodo.type]: (state, action) => state.filter(toDo => toDo.id !== action.payload);
+})
 
 export const actionCreators = {
   addTodo,
